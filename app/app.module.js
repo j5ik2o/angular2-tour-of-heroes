88ff685876e2fc69c39e3af141794a86eee94a4c
Junichi Kato <j5ik2o@gmail.com>
2016-08-23 09:55:03 +0900

    Added support for routing

--- app/app.module.js
+++ app/app.module.js
@@ -5,29 +5,43 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
-var hello_world_component_1 = require('./hello-world.component');
+var app_routing_1 = require('./app.routing');
+var heroes_component_1 = require('./heroes.component');
+var dashboard_component_1 = require('./dashboard.component');
+var hero_detail_component_1 = require('./hero-detail.component');
+var hero_service_1 = require('./hero.service');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [
-                platform_browser_1.BrowserModule
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+                app_routing_1.routing
             ],
             declarations: [
                 app_component_1.AppComponent,
-                hello_world_component_1.HelloWorldComponent
+                heroes_component_1.HeroesComponent,
+                dashboard_component_1.DashboardComponent,
+                hero_detail_component_1.HeroDetailComponent
+            ],
+            providers: [
+                hero_service_1.HeroService
             ],
             bootstrap: [app_component_1.AppComponent]
-        }), 
-        __metadata('design:paramtypes', [])
+        })
     ], AppModule);
     return AppModule;
 }());
 exports.AppModule = AppModule;
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */ 
+//# sourceMappingURL=app.module.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
 var path = require('path');
-
+var helpers = require('./config/helpers');
 module.exports = {
     entry: {
-        index :'./app/index.ts'
+        index: './app/index.ts'
     },
     output: {
         path: './dist',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './',
         port: 3000
     },
     resolve: {
-        root:[path.join(__dirname,'node_modules')],
-        extensions:['', '.ts', '.webpack.js', '.web.js', '.js', '.html']
+        root: [path.join(__dirname, 'node_modules')],
+        extensions: ['', '.ts', '.webpack.js', '.web.js', '.js', '.html']
     },
     module: {
         loaders: [
-            { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loaders: ['to-string-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'raw-loader',
+                exclude: [helpers.root('./index.html')]
+            },
         ]
     }
 };
